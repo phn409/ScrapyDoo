b506073b2b66c5b1f60f639b71b9f0f7dadc3a05
phn409 <phn1@ualberta.ca>
2019-08-26 17:51:49 -0600

    new script and revised APS class in scripts folder

---
+++ scripts/APS.py
@@ -0,0 +1,173 @@
+"""
+scraper to pull journal info from APS journals
+"""
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# holds information about an individual volume
+class volume:
+    """
+    small class to hold information about an individual volume
+    """
+    # initialize object
+    def __init__(self):
+        self.nIssues=0
+        return
+    # index all issues in this volume
+    def indexIssues(self,url):
+        html=urlopen(url)
+        soup=BeautifulSoup(html,'html.parser')
+        features=soup.find_all('b')
+        self.nIssues=len(features)
+        
+        return
+
+# Class to hold information involving all APS journals
+# input abbreviation (e.g., PRL, PRB etc)
+class APS:
+    """
+    aps journal main class
+    """
+    def __init__(self,abbrev,url='https://journals.aps.org/'):
+        """
+        initialize class
+        
+        Notes
+        -----
+        This is still incomplete
+
+        Parameters
+        ----------
+        abbrev : journal abbreviation
+        url : base url of journal
+        """
+        self.abbrev=abbrev
+        self.baseurl=url+abbrev+'/'
+        self.volumeurl=url+abbrev+'/issues/'
+        return
+    
+    # scrape the information about # of volumes
+    def indexVolumes(self):
+        html=urlopen(self.volumeurl)
+        soup=BeautifulSoup(html,'html.parser')
+
+        features=soup.find_all('a',href=True)
+        self.volumes={}
+        self.nVolumes=0
+        for f in features:
+            if '/'+self.abbrev+'/issues/' in f['href']:
+                string=f['href'].strip('/'+self.abbrev+'/issues/')
+                loc=string.find('#')
+                if loc != -1:
+                    self.volumes.update({string[:loc]:volume()})
+                    self.nVolumes=self.nVolumes+1
+
+        return
+    # scrape info about number of issues in each volume
+    # if All is true, it indexes all volumes, otherwise only volume num
+    # verbose gives status updates, skipping (skip) # of steps
+    def indexIssues(self,All=True,num=1,verbose=False,skip=1):
+        self.nIssues=0
+        if All:
+            n=0
+            for i in self.volumes:
+                if verbose and n%skip==0:
+                    print(str(n)+'/'+str(len(self.volumes)))
+                n=n+1
+                url=self.volumeurl+i+'#v'+i
+                self.volumes[i].indexIssues(url)
+                self.nIssues=self.nIssues+self.volumes[i].nIssues
+            return
+        else:
+            url=self.volumeurl+str(num)+'#v'+str(num)
+            self.volumes[str(num)].indexIssues(url)
+            
+            return
+    
+    # print a link to a chosen volume/issue
+    # if vol==False, links to the current page
+    def link(self,vol=False,iss=1):
+        if vol:
+            return self.volumeurl+str(vol)+'/'+str(iss)
+        else:
+            return self.baseurl
+
+    #Functions to scrape an issue to retrive all article information.
+    #Input is base URL. Output is list of article names, corresponding 
+    #authors, DOI, keywords, etc.
+    
+    #Function to get the months range and year for the volume/issue
+    def monthRange(self, v, i):
+        issURL = self.link(vol = v, iss = i )
+        html=urlopen(issURL)
+        soup=BeautifulSoup(html,'html.parser')
+
+        head = soup.find_all('div',class_="panel")
+        rng = head[0].find('h5').get_text().split()
+#        months = rng.getText()
+#        print(head[0])
+#        print(rng.get_text())
+        print(' '.join(rng))
+        #String manipulation to extract month range and year
+        mos = rng[0] + ' - ' + rng[2]
+        yr = rng[-1]
+        return [mos, yr]
+    
+    #Method to get issue titles
+    def issueListing(self, v, i):
+        """
+        Lists out all the papers in the specified volume and issue.
+        Inputs:
+        v: journal volume number
+        i: journal issue number
+        """
+        issURL = self.link(vol = v, iss = i )
+        html=urlopen(issURL)
+        soup=BeautifulSoup(html,'html.parser')
+        
+        titles = soup.find_all('h5', class_="title")
+        authors = soup.find_all('h6', class_="authors")
+        pubs = soup.find_all('h6', class_="pub-info")
+        for t, a, p in zip(titles, authors, pubs):
+            #Extract abstract url from title head
+            aURL = t.find('a', href = True)['href']
+            alink = 'https://journals.aps.org' + aURL
+            print(t.get_text())
+            print(a.get_text())
+            print(p.get_text())
+            print(alink)
+            print('----------------------------------------------------------------')
+        
+        return
+#    def articleAuthors():
+#    """
+#    Method to extract authors. 
+#    Adds new author name to Authors database.
+#    """
+    
+    def articleKeywords(self, v, i):
+        """
+        Method to extract keywords. 
+        Adds new keywords to Keywords database.
+        """
+        issURL = self.link(vol = v, iss = i )
+        html=urlopen(issURL)
+        soup=BeautifulSoup(html,'html.parser')
+        keylists = soup.find_all('ul', class_="inline-list subjects")
+        for ks in keylists:
+            kw = ks.find_all('li')
+            keywords = [li.get_text() for li in kw]
+            print(keywords)
+        return
+
+    
+#class article:
+    """
+    Article class 
+    Attributes: Paper ID, url, title, authors, journal, year, volume, issue, article number,
+    page numbers, keywords, DOI number,  
+    Methods: abstract, formatted citation, citations to, citations from
+    """
+    #Pass on inherited values from the upper classes
+#    def __init__(self):
--- scripts/PRX.py
+++ scripts/PRX.py
@@ -1,4 +1,5 @@
 from APS import APS
+import pandas as pd
 
 PRX=APS(abbrev='prx')
 PRX.indexVolumes()
@@ -8,3 +9,33 @@ print('link current issue:',PRX.link())
 print('link volume 3 issue 2:',PRX.link(vol=2,iss=4))
 print('total number of volumes:',PRX.nVolumes)
 print('total number of issues:',PRX.nIssues)
+
+#%%
+
+vol_iss = {'Vol': list(reversed([*PRX.volumes,])), 'Iss':[], 'Yr':[]}
+for v in vol_iss['Vol']:
+    #Print number of issues in each volume
+    PRX.indexIssues(All = False, num = int(v))
+    ni = PRX.volumes[v].nIssues
+    vol_iss['Iss'].append(ni)
+    print('Volume: {}, Issues: {}'.format(v, ni))
+    for i in range(1, ni+1):
+        print(PRX.link(vol=v, iss=i))
+        r = PRX.monthRange(v, i)
+    #Append to year column
+    vol_iss['Yr'].append(r[1])        
+VIDF = pd.DataFrame(vol_iss)
+print(VIDF)
+#%%
+#Section to scrape a particular volume of a journal
+#Print out the issue URL corresponding to each volume/issue
+VL = 9
+#for i in range(1, PRX.volumes[str(VL)].nIssues + 1):
+print(PRX.link(vol=VL, iss=1))
+PRX.monthRange(9, 1)    
+PRX.issueListing(9, 1)
+#to DF
+#VIDF = pd.DataFrame(vol_iss)
+#print(VIDF)
+    
+#Create paper objects with index ID
